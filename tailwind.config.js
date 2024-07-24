8457641ec695199d54f0bb9e51918ef5a5641e0c
dieramirezma <dieramirezma@unal.edu.co>
2024-07-23 19:35:01 -0500

    chore: Update ESLint configuration and package dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
+import { nextui } from '@nextui-org/react'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        logo: '#009ED8',
+        primary: {
+          500: '#1570EF',
+          600: '#1366D9',
+          700: '#0F50AA'
+        },
+        grey: {
+          50: '#F0F1F3',
+          100: '#D0D3D9',
+          300: '#989FAD',
+          500: '#667085',
+          600: '#5D6679',
+          700: '#48505E',
+          900: '#2B2F38'
+        }
+
+      }
+    }
   },
-  plugins: [],
-};
+  darkMode: 'class',
+  plugins: [nextui()]
+}
